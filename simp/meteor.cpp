2985e43733ee3f55907822a8c98cda1025aab406
juanpablo <juanriverogaray@gmail.com>
2024-11-27 17:56:26 -0500

    agregar clase meteor

---
+++ simp/meteor.cpp
@@ -0,0 +1,122 @@
+#include "meteor.h"
+
+meteor::meteor(unsigned int scale)
+{
+    pixmap_management = new sprite(":/asteroid.png",scale);
+    pixmap_management->cut_character_pixmap(set_complete_sprites1());
+    pixmap_management->set_design_size(meteor_pixel_x_size,meteor_pixel_y_size);
+
+    explosion_management = new sprite(":/explotion.png", scale);
+    explosion_management->cut_character_pixmap(set_complete_sprites2());
+    explosion_management->set_design_size(explo_pixel_x_size,explo_pixel_y_size);
+
+    //setPixmap();
+    set_animations();
+    setPixmap(pixmap_management->get_current_pixmap(0));
+    y_dimension = 4; // Escala vertical
+    this->vy = 0;    // Velocidad inicial en el eje Y
+}
+
+meteor::~meteor()
+{
+    delete pixmap_management;
+    delete explosion_management;
+}
+float meteor::get_vy()
+{
+    return vy;
+}
+
+void meteor::set_initial_conditions(float x, float y, float vy)
+{
+    this->x = x;   // Posición inicial en X (fija)
+    this->y = y;   // Posición inicial en Y
+    this->vy = vy; // Velocidad inicial en Y
+    setPos(x, y);  // Establece la posición gráfica inicial
+}
+
+
+void meteor::time_step(bool can_move)
+{
+    if (can_move)
+    {
+        // Actualiza la velocidad debido a la gravedad
+        vy += acc * time_period / 1000.0;
+        y += vy * time_period / 1000.0;
+        setPos(x, y);
+    }
+    else
+    {
+        // Si no puede moverse, manejar intentos o explotar
+        attempt_count++;
+        if (attempt_count < max_attempts)
+        {
+            // Cambiar el sprite del meteorito
+            setPixmap(explosion_management->get_current_pixmaps(0,explo_pixel_x_size,explo_pixel_y_size));
+            setPos(x, y + 10);
+        }
+        else
+        {
+            emit start_explosion();
+        }
+    }
+}
+
+
+
+QRect meteor::set_complete_sprites1()
+{
+    QRect dim;
+
+    dim.setX(0);
+    dim.setY(0);
+    dim.setHeight(6*meteor_pixel_y_size);
+    dim.setWidth(3*meteor_pixel_x_size);
+
+    return dim;
+}
+
+void meteor::set_animations()
+{
+    set_down_animation();
+    set_explo_animation1();
+}
+
+void meteor::set_down_animation()
+{
+    QRect dim;
+
+    dim.setX(0);
+    dim.setY(0);
+    dim.setHeight(1*meteor_pixel_y_size);
+    dim.setWidth(1*meteor_pixel_x_size);
+
+    pixmap_management->add_new_animation(dim,1);
+}
+QRect meteor::set_complete_sprites2()
+{
+    QRect dime;
+
+    dime.setX(0);
+    dime.setY(0);
+    dime.setHeight(2*explo_pixel_y_size);
+    dime.setWidth(5*explo_pixel_x_size);
+
+    return dime;
+}
+
+void meteor::set_explo_animation1()
+{
+    QRect dime;
+
+    dime.setX(0);
+    dime.setY(0);
+    dime.setHeight(1*explo_pixel_y_size);
+    dime.setWidth(5*explo_pixel_x_size);
+
+
+   explosion_management->add_new_animation(dime,5);
+}
+
+
+
---
+++ simp/meteor.h
@@ -0,0 +1,53 @@
+#ifndef METEOR_H
+#define METEOR_H
+
+#include <QObject>
+#include <QGraphicsPixmapItem>
+#include "sprite.h"
+#include <QTimer>
+
+#define meteor_pixel_x_size 19
+#define meteor_pixel_y_size 18
+
+#define explo_pixel_x_size 190
+#define explo_pixel_y_size 200
+
+class meteor : public QObject, public QGraphicsPixmapItem
+{
+    Q_OBJECT
+public:
+    meteor(unsigned int scale);
+    ~meteor();
+    void set_initial_conditions(float x, float y, float vy);
+    float get_vy();
+signals:
+    void start_explosion();
+
+public slots:
+    void time_step(bool can_move);
+
+private:
+    QRect set_complete_sprites1();
+    void set_animations();
+    void set_down_animation();
+
+    QRect set_complete_sprites2();
+    void set_explo_animation1();
+    void set_explo_animation2();
+    // Atributos para manejar la simulacion de caida libre
+    float x, y;
+    float vy;
+    const float acc = 9.8;
+    QTimer *time;        // Temporizador para actualizar el movimiento
+    int time_period=100;     // (en ms)
+    float y_dimension;   // Escala para la posición vertical
+    // Manejo de sprites
+    sprite *pixmap_management;
+    sprite *explosion_management;
+    int attempt_count = 0;
+    const int max_attempts = 5;
+
+};
+
+
+#endif // METEOR_H
